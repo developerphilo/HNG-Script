ef34e9bfacd671c8bfe145a3dbaa2c8b74fec329
John Philip <60175531+developerphilo@users.noreply.github.com>
2020-06-03 19:18:09 +0300

    Create John-Philip.js

---
+++ scripts/John-Philip.js
@@ -0,0 +1,5 @@
+const info =["John Philip", "HNG-01932", "Javascript", "developerphilo@gmail.com"];
+
+console.log(
+  `Hello World, this is ${info[0]} with HNGi7 ID ${info[1]} and email ${info[3]} using ${info[2]} for stage 2 task`
+);
